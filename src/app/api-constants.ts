c6181a932dac1190be94d7c3ddd8a1026ed6110e
cvelascofa <cvelascofa@uoc.edu>
2025-04-27 21:07:34 +0200

    Modify AUTH_API url.

--- src/app/api-constants.ts
+++ src/app/api-constants.ts
@@ -1,2 +1,2 @@
 //export const AUTH_API = 'http://localhost:8080/';
-export const AUTH_API = 'http://cvelascofa-pageturner-backend-production.up.railway.app/';
+export const AUTH_API = 'https://cvelascofa-pageturner-backend-production.up.railway.app/';
