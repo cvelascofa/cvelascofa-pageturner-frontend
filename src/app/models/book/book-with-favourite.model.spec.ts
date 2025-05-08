933654ae8f50af82bdafab0425ba7a04578777c3
Carla Velasco <cvelascofa@uoc.edu>
2025-05-08 22:08:03 +0200

    Functional create delete and get by user entity Favourites.

---
+++ src/app/models/book/book-with-favourite.model.spec.ts
@@ -0,0 +1,7 @@
+import { BookWithFavourite } from './book-with-favourite.model';
+
+describe('BookWithFavourite', () => {
+  it('should create an instance', () => {
+    expect(new BookWithFavourite()).toBeTruthy();
+  });
+});
